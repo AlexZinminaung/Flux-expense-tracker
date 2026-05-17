c51d358dfa58d75dedf9b0b239a6bdab272afd71
Zinmin Aung <zinminaung@Zinmins-MacBook-Air.local>
2026-05-17 07:32:31 +0700

    add vite pwd

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+      react(), 
+      tailwindcss(), 
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "Expense Tracker",
+          short_name: "Expenses",
+          start_url: "/",
+          display: "standalone",
+          theme_color: "#000000",
+          icons: [
+            {
+              src: "/icon.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/icon.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      })]
 })
